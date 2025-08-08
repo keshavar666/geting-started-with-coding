d9bcbbd92a31631fac510fdab0b90aa479097681
keshavar666 <isurika.peiris@gmail.com>
2025-08-08 19:46:06 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,5 @@
+print("my name is keshavar")
+
+print("i am 11 years old")
+
+print("i love coding")
---
+++ activity2.py
@@ -0,0 +1,7 @@
+x=5
+y="John"
+print(x)
+print(y)
+name=input("enter your name:")
+print(name)
+
