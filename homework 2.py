b73f91cf6c1f6a2c07f46a09906b6c77de568b15
keshavar666 <isurika.peiris@gmail.com>
2025-08-08 20:26:55 +0530

    Add files via upload

---
+++ homework 2.py
@@ -0,0 +1,11 @@
+friend1="april 1st"
+friend2="january 3rd"
+friend3="december 22nd"
+friend4="may 2nd"
+friend5="july 3rd"
+
+print(friend1)
+print(friend2)
+print(friend3)
+print(friend4)
+print(friend5)
